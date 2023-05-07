79a9fa278c8ad585acd499ea871fa3a0976f5bc4
123 <123@DESKTOP-VSNJ2KA>
2023-05-07 22:53:31 +0300

    Добавьте файлы проекта.

---
+++ Pr.12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word = "каміння"; // замість "каміння" можна вказати будь-яке інше слово
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = i + 1; j < word.Length; j++)
+            {
+                if (word[i] == word[j])
+                {
+                    Console.WriteLine("Однаковi лiтери: " + word[i]);
+                    Console.WriteLine("Позицiї: " + i + ", " + j);
+                }
+            }
+        }
+    }
+}
